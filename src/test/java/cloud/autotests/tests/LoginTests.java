d4fdf8650aae0a58f929064524d94bfb22ec5df8
svasenkov <aanher@gmail.com>
2021-04-16 20:56:46 +0300

    fixed dependency

--- src/test/java/cloud/autotests/tests/LoginTests.java
+++ src/test/java/cloud/autotests/tests/LoginTests.java
@@ -3,15 +3,11 @@ package cloud.autotests.tests;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 
-import static cloud.autotests.api.LogFilter.filters;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.given;
 
 @Story("Login tests")
 public class LoginTests extends TestBase {
